e63a4c02fd14e69c65bd9399942b84832e66aa05
TooniOlaniyan <asalamlatif@gmail.com>
2024-02-27 16:07:29 +0100

    All assets collection

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-orange": "#F05600",
+        "light-orange": "#FEA700",
+        "test-black": "#02060C",
+        "primary-grey": "#F0F0F5",
+      },
+    },
   },
   plugins: [],
 };
